468aae358bfe614fff7c28a1ba28c7920c455c5c
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-09-20 21:05:34 -0400

    Component: DisplayTotal

--- src/components/displayTotal/displayTotal.jsx
+++ src/components/displayTotal/displayTotal.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import AssetLineItem from '../assetLineItem/assetLineItem';
-
+import styles from './displayTotal.module.css';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const DisplayTotal = ({assets}) => {
   return (
-    <section>
-      <h1>Assets</h1>
-      {assets.map(asset => (
-        <AssetLineItem asset={asset}/>
-      ))}
+    <section className={styles.DisplayTotal}>
+      <h1 className={styles.title}>Assets</h1>
+      <div className={styles.table_container}>
+        <Table className={styles.table}  bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Name</th>
+              <th>Avg Price</th>
+              <th>Shares</th>
+              <th>Input</th>
+              <th>Goal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {assets.map(asset => (
+              <AssetLineItem asset={asset}/>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </section>
   );
 }
