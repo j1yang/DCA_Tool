9e7bc64e280e204e0f6251be819af88730a1cebc
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-09-20 21:05:56 -0400

    Component: Row display

--- src/components/assetLineItem/assetLineItem.jsx
+++ src/components/assetLineItem/assetLineItem.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import styles from './assetLineItem.module.css';
 
 const AssetLineItem = ({asset}) => {
   const {id, tickerSymbol, name, averagePrice, shares,input, goal} = asset;
 
   return (
-    <p>{tickerSymbol} | {name} | {averagePrice} | {shares} | {input} | {goal}</p>
+    <tr className={styles.row}>
+      <td className={styles.tickerSymbol}>{tickerSymbol}</td>
+      <td className={styles.name}>{name}</td>
+      <td className={styles.averagePrice}>{averagePrice}</td>
+      <td className={styles.shares}>{shares}</td>
+      <td className={styles.input}>{input}</td>
+      <td className={styles.goal}>{goal}</td>
+    </tr>
   );
 };
 
