dcc103a38cdbc124e02fce9e76ada234e8dda87f
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-09-24 16:57:20 -0400

    ProfitService Correction

--- src/services/profit.js
+++ src/services/profit.js
@@ -1,26 +1,31 @@
-const calculate = (asset, price) =>{
-  const currentValue = 3.1 * asset.shares;
-  const earning = 
-  (asset.input > currentValue) ? 
-  (asset.input - currentValue)
-  :(currentValue - asset.input);
-  const sign = (earning >= 0) ? '+' : '-';
-  const percentage = 
-  ((currentValue - asset.input) >= 0) 
-  ? ((1-asset.input / currentValue) * 100)
-  : (-(1-currentValue / asset.input) * 100 );
+class ProfitService{
 
-  const calculation = {
-    name: asset.name,
-    symbol: asset.tickerSymbol,
-    quantity: asset.shares,
-    purchasedValue: asset.input,
-    currentValue: currentValue,
-    sign: sign,
-    earning: earning,
-    percent: percentage,
-    quote: price
-  }
+  calculate = (asset, price) =>{
+    const currentValue = price * asset.shares;
+    const earning = 
+    (asset.input < currentValue) ? 
+    (currentValue - asset.input):(currentValue - asset.input);
+    const sign = (earning >= 0) ? '+' : '-';
+    const percentage = 
+    ((currentValue - asset.input) >= 0) 
+    ? ((currentValue / asset.input) * 100-100)
+    : ((1-currentValue / asset.input) * 100 );
 
-  return calculation;
-};
+    const calculation = {
+      name: asset.name,
+      symbol: asset.tickerSymbol,
+      quantity: asset.shares.toFixed(4),
+      purchasedValue: asset.input.toFixed(2),
+      currentValue: currentValue.toFixed(2),
+      sign: sign,
+      earning: Math.abs(earning.toFixed(2)),
+      percent: percentage.toFixed(2),
+      quote: price.toFixed(8)
+    }
+
+    return calculation;
+  };
+
+}
+
+export default ProfitService;
