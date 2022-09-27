da0f572ade9aa2b82e13ddff21a836bf307a5cb9
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-09-26 23:24:13 -0400

    CSS: DisplayProfits

--- src/components/displayProfits/displayProfits.jsx
+++ src/components/displayProfits/displayProfits.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ProfitLineItem from '../profitLineItem/profitLineItem';
+import styles from './displayProfits.module.css';
 const DisplayProfits = ({profits}) => {
   return (
-    <section>
+    <section className={styles.profits}>
+      <h2 className={styles.title}>Calculation</h2>
       <ul>
+        <div className={styles.category}>
+        <li className={styles.name}>Name</li>
+          <li className={styles.symbol}>Symbol </li>
+          {/* <li className={styles.quantity}>Quantity</li> */}
+          <li className={styles.purchased}>Purchased Value</li>
+          <li className={styles.current}>Current Value</li>
+          <li className={styles.earning}>Earning</li>
+          <li className={styles.percentage}>%</li>
+        </div>
         {profits.map((profit)=>(
           <ProfitLineItem profit={profit}/>
         ))}
