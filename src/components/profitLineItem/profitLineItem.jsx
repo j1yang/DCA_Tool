39afb8ac950debe3c8bcee8d75a9abef08e68aa5
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-09-26 23:24:31 -0400

    CSS: ProfitLineItem

--- src/components/profitLineItem/profitLineItem.jsx
+++ src/components/profitLineItem/profitLineItem.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-
+import styles from './profitLineItem.module.css';
 const ProfitLineItem = ({profit}) => {
   const {name, symbol, quantity, purchasedValue,
     currentValue,sign, earning,percent, quote} = profit;
-  {console.log(profit)}
   return(
     <>
-      <li>Symbol: {symbol}</li>
-      <li>Quantity: {quantity}{symbol}</li>
-      <li>Purchased Value: ${purchasedValue}</li>
-      <li>Current Value: ${currentValue}</li>
-      <li>Earning: {sign}${earning}</li>
-      <li>%: {sign}{percent}%</li>
-    </>
+    
+    <div className={styles.line}>
+      <li className={styles.name}>{name}</li>
+      <li className={styles.symbol}>{symbol}</li>
+      {/* <li className={styles.quantity}>{quantity}{symbol}</li> */}
+      <li className={styles.purchased}>${purchasedValue}</li>
+      <li className={styles.current}>${currentValue}</li>
+      <li className={styles.earning}>{sign}${earning}</li>
+      <li className={styles.percentage}>{sign}{percent}%</li>
+    </div></>
   );
 };
 
