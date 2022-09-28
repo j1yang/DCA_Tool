972f5ab6694fd0da72bb21fe562d812ad371fc24
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-09-27 21:22:09 -0400

    fontawesome added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import ProfitService from './services/profit';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 const profitService = new ProfitService();
 
