8b0678c1a342c4f11a3f9510ef49cd45817a3753
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-09-28 21:14:01 -0400

    CSS: transaction history line item

--- src/components/recordLineItem/recordLineItem.jsx
+++ src/components/recordLineItem/recordLineItem.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
+import styles from './recordLineitem.module.css';
 const RecordLineItem = ({filtered}) => {
   const {id, assetId, date, price, amount, shares} = filtered;
 
-  
   return(
-    <div>
-      <li>{date}</li>
-      <li>{price}</li>
-      <li>{amount}</li>
-      <li>{shares}</li>
+    <div className={styles.line} >
+      <li className={styles.date}>{date}</li>
+      <li className={styles.price}>${price.toFixed(4)}</li>
+      <li className={styles.amount}>${amount.toFixed(2)}</li>
+      <li className={styles.shares}>{shares.toFixed(4)}</li>
     </div>
   );
 };
