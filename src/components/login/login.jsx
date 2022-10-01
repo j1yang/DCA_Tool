114a469865bb4bb8e5731c02aed5e7355ebce918
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-10-01 16:08:34 -0400

    Component Login

---
+++ src/components/login/login.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import styles from './login.module.css';
+
+const Login = ({authService}) => {
+  const navigate = useNavigate();
+
+  const goToMain = userId => {
+
+    navigate(
+      "/main", 
+      { 
+        state: {
+          userId:userId
+        }
+      });
+  };
+
+  const onLogin = (event) => {
+    authService
+    .login(event.currentTarget.textContent)
+    .then(data => goToMain(data.user.uid));
+  };
+
+  useEffect (()=> {
+    authService
+    .onAuthChange(user => {
+      user && goToMain(user.id);
+    });
+  });
+
+  return (
+    <section className={styles.login}>
+      <section>
+        <h1>login</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>Google</button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>Github</button>
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
+}
+
+export default Login;
