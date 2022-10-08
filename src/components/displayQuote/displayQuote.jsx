230696e3eac83df9c7462f4a9c05628c0969a6d8
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-10-07 20:21:52 -0400

    +/- value colour changing

--- src/components/displayQuote/displayQuote.jsx
+++ src/components/displayQuote/displayQuote.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './displayQuote.module.css';
 const DisplayQuote = ({coin}) => {
   const {name , symbol , price, priceChange} = coin;
-
-  
+  const colour = priceChange >= 0 ? styles.green : styles.red;
+  const sign = priceChange >= 0 ? '+' : '';
   return(
     <div className={styles.line}>
       <li className={styles.name}>{name}</li>
       <li className={styles.symbol}>{symbol}</li>
       <li className={styles.price}>${price}</li>
-      <li className={styles.priceChange}>{priceChange}%</li>
+      <li className={`${styles.priceChange} ${colour}`}>{sign}{priceChange}%</li>
     </div>
   );
 };
--- src/components/profitLineItem/profitLineItem.jsx
+++ src/components/profitLineItem/profitLineItem.jsx
@@ -3,6 +3,8 @@ import styles from './profitLineItem.module.css';
 const ProfitLineItem = ({profit}) => {
   const {name, symbol, quantity, purchasedValue,
     currentValue,sign, earning,percent, quote} = profit;
+    const colour = sign === '+' ? styles.green : styles.red;
+
   return(
     <div className={styles.line}>
       <li className={styles.name}>{name}</li>
@@ -10,8 +12,8 @@ const ProfitLineItem = ({profit}) => {
       {/* <li className={styles.quantity}>{quantity}{symbol}</li> */}
       <li className={styles.purchased}>${purchasedValue}</li>
       <li className={styles.current}>${currentValue}</li>
-      <li className={styles.earning}>{sign}${earning}</li>
-      <li className={styles.percentage}>{sign}{percent}%</li>
+      <li className={`${styles.earning} ${colour}`}>{sign}${earning}</li>
+      <li className={`${styles.percentage} ${colour}`}>{sign}{percent}%</li>
     </div>
   );
 };
