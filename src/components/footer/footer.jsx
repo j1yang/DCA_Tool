029451e3cac130a58c234de17c596a002c1064af
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-10-07 20:51:03 -0400

    footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = (props) => <p className={styles.footer}>
+Copyright ©️ 2022 JaewonYang All Rights Reserved
+</p>;
+
+export default Footer;
